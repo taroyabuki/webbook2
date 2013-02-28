b3a1bc2d839dce385db3d50462e6b7dfde79ef0f
Taro YABUKI <taro.yabuki@unfindable.net>
2013-03-01 00:41:21 +0900

    public_timeline

---
+++ src/05/public_timeline/json.php
@@ -0,0 +1,36 @@
+<?php
+//This script emulates https://dev.twitter.com/docs/api/1/get/statuses/public_timeline.json.
+//Options except callback are not supported.
+
+require_once 'OAuth.php';
+$api = 'https://stream.twitter.com/1.1/statuses/sample.json';
+$consumer = new OAuthConsumer(
+        '***** Consumer key *****',
+        '***** Consumer secret *****');
+$token = new OAuthConsumer(
+        '***** Consumer secret *****',
+        '***** Token secret *****');
+$request = OAuthRequest::from_consumer_and_token($consumer, $token, 'GET', $api);
+$request->sign_request(new OAuthSignatureMethod_HMAC_SHA1(), $consumer, $token);
+
+//stream_context_set_default(array('http' => array('proxy' => "tcp://proxy.example.net:3128")));
+$stream = fopen($request->to_url(), 'r') or die();
+
+$result = '[';
+$i = 0;
+while ($i < 20 && $json = fgets($stream)) {
+  $status = json_decode($json, true);
+  if (isset($status['text'])) {
+    $result.="$json,";
+    $i++;
+  }
+}
+$result = rtrim($result, ",") . ']';
+
+if (isset($_GET['callback'])) {
+  header("Content-Type:application/javascript; charset=utf-8");
+  printf("%s(%s)", $_GET['callback'], $result);
+} else {
+  header("Content-Type:application/json; charset=utf-8");
+  echo $result;
+}
---
+++ src/05/public_timeline/xml.php
@@ -0,0 +1,36 @@
+<?php
+
+//This script emulates https://dev.twitter.com/docs/api/1/get/statuses/public_timeline.xml.
+//Options except callback are not supported.
+
+require_once 'OAuth.php';
+$api = 'https://stream.twitter.com/1.1/statuses/sample.json';
+$consumer = new OAuthConsumer(
+        '***** Consumer key *****',
+        '***** Consumer secret *****');
+$token = new OAuthConsumer(
+        '***** Consumer secret *****',
+        '***** Token secret *****');
+$request = OAuthRequest::from_consumer_and_token($consumer, $token, 'GET', $api);
+$request->sign_request(new OAuthSignatureMethod_HMAC_SHA1(), $consumer, $token);
+
+//stream_context_set_default(array('http' => array('proxy' => "tcp://proxy.example.net:3128")));
+$stream = fopen($request->to_url(), 'r') or die();
+
+error_reporting(E_ALL ^ E_STRICT); //for XML/Serializer
+require_once 'XML/Serializer.php';
+$result = '';
+$i = 0;
+while ($i < 20 && $json = fgets($stream)) {
+  $status = json_decode($json, true, 512, JSON_BIGINT_AS_STRING);
+  if (isset($status['text'])) {
+    $serializer = new XML_Serializer(array('rootName' => 'status'));
+    $serializer->serialize($status);
+    $result.=$serializer->getSerializedData();
+    $i++;
+  }
+}
+
+header("Content-Type:text/xml; charset=utf-8");
+echo "<?xml version='1.0' encoding='UTF-8'?>\n";
+echo "<statuses type='array'>$result</statuses>";

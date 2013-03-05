01d358d3d7e40aab86536d7510df945afc6bf838
Taro YABUKI <jr.tr.ybk@gmail.com>
2013-03-05 16:06:50 +0900

    Update json.php

--- src/05/public_timeline/json.php
+++ src/05/public_timeline/json.php
@@ -2,23 +2,12 @@
 //This script emulates https://dev.twitter.com/docs/api/1/get/statuses/public_timeline.json.
 //Options except callback are not supported.
 
-require_once 'OAuth.php';
-$api = 'https://stream.twitter.com/1.1/statuses/sample.json';
-$consumer = new OAuthConsumer(
-        '***** Consumer key *****',
-        '***** Consumer secret *****');
-$token = new OAuthConsumer(
-        '***** Consumer secret *****',
-        '***** Token secret *****');
-$request = OAuthRequest::from_consumer_and_token($consumer, $token, 'GET', $api);
-$request->sign_request(new OAuthSignatureMethod_HMAC_SHA1(), $consumer, $token);
-
-//stream_context_set_default(array('http' => array('proxy' => "tcp://proxy.example.net:3128")));
-$stream = fopen($request->to_url(), 'r') or die();
+require_once 'MyStream.php';
+$myStream = new MyStream();
 
 $result = '[';
 $i = 0;
-while ($i < 20 && $json = fgets($stream)) {
+while ($i < 20 && $json = fgets($myStream->getStream())) {
   $status = json_decode($json, true);
   if (isset($status['text'])) {
     $result.="$json,";

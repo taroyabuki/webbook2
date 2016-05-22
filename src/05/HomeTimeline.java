bb0f4b798afb5e216f0e230db95f00597b19c51e
Taro YABUKI <taro.yabuki@it-chiba.ac.jp>
2016-05-22 14:13:44 +0900

    微修正

--- src/05/HomeTimeline.java
+++ src/05/HomeTimeline.java
@@ -6,26 +6,26 @@ import jdk.nashorn.internal.runtime.*;
 
 public class HomeTimeline {
 
-	public static void main(String[] args) throws Exception {
-		URL url = new URL("http://localhost/phpweb/home_timeline.json.php");
-		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-		BufferedReader br = new BufferedReader(
-						new InputStreamReader(connection.getInputStream(), "UTF-8"));
-		StringBuilder json = new StringBuilder();
-		String line;
-		while ((line = br.readLine()) != null) {
-			json.append(line);
-		}
+  public static void main(String[] args) throws Exception {
+    URL url = new URL("http://localhost/phpweb/home_timeline.json.php");
+    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+    BufferedReader br = new BufferedReader(
+            new InputStreamReader(connection.getInputStream(), "UTF-8"));
+    StringBuilder json = new StringBuilder();
+    String line;
+    while ((line = br.readLine()) != null) {
+      json.append(line);
+    }
 
-		ScriptEngineManager sem = new ScriptEngineManager();
-		ScriptEngine engine = sem.getEngineByName("JavaScript");
-		String script = "Java.asJSONCompatible(" + json + ")";
-		for (Object element : (JSONListAdapter) engine.eval(script)) {
-			Map<String, Object> tweet = (Map<String, Object>) element;
+    ScriptEngineManager sem = new ScriptEngineManager();
+    ScriptEngine engine = sem.getEngineByName("JavaScript");
+    String script = "Java.asJSONCompatible(" + json + ")";
+    for (Object element : (JSONListAdapter) engine.eval(script)) {
+      Map<String, Object> tweet = (Map<String, Object>) element;
 //      for (Map.Entry<String, Object> entry: tweet.entrySet()) {
 //        System.out.println(entry.getKey() + ": " + entry.getValue());
 //      }
-			System.out.println(tweet.get("text"));
-		}
-	}
+      System.out.println(tweet.get("text"));
+    }
+  }
 }
